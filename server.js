f21646d4525a02ec5ef9a67c6f449db9db018bd5
ManavPandya31 <manavpandya42@gmail.com>
2026-02-27 18:03:43 +0530

    portfolio

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./db.js";
+import contactRoutes from "./routes/contactRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+// app.use(cors({
+//   origin: [
+//     "http://localhost:5173",
+//     "https://your-netlify-url.netlify.app"
+//   ],
+//   credentials: true
+// }));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contact", contactRoutes);
+
+const PORT = process.env.PORT || 3131;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
